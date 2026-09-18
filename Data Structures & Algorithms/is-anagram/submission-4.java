4d8d59e018878c1686d7037d0aa050b6fa956afd
bennbenben <80408902+bennbenben@users.noreply.github.com>
2026-09-19 01:42:24 +0800

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.java
@@ -0,0 +1,19 @@
+class Solution {
+  public boolean isAnagram(String s, String t) {
+    if (s.length()!=t.length()) {
+      return false;
+    }
+    char[] sCharArray = s.toCharArray();
+    Arrays.sort(sCharArray);
+    
+    char[] tCharArray = t.toCharArray();
+    Arrays.sort(tCharArray);
+    
+    for (int i=0; i<s.length(); i++) {
+      if (sCharArray[i] != tCharArray[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
