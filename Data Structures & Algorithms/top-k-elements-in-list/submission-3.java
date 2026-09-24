58cd56bd89eeff29c6c4d5ab34eba67f7ea117f1
bennbenben <80408902+bennbenben@users.noreply.github.com>
2026-09-24 12:03:08 +0800

    Add: top-k-elements-in-list - submission-3

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-3.java
@@ -0,0 +1,26 @@
+class Solution {
+  public int[] topKFrequent(int[] nums, int k) {
+    Map<Integer, Integer> frequencyMap = new HashMap<Integer, Integer>();
+    
+    for (int num : nums) {
+      int numExistingCount = frequencyMap.getOrDefault(num, 0);
+      frequencyMap.put(num, numExistingCount + 1);
+    }
+    
+    int[][] frequencyArray = new int[frequencyMap.size()][2];
+    int i = 0;
+    for (Map.Entry<Integer, Integer> entry : frequencyMap.entrySet()) {
+      frequencyArray[i][0] = entry.getKey();
+      frequencyArray[i][1] = entry.getValue();
+      i += 1;
+    }
+    
+    Arrays.sort(frequencyArray, (a, b) -> b[1] - a[1]);
+    int[] topKElements = new int[k];
+    for (int j=0; j< k; j++) {
+      topKElements[j] = frequencyArray[j][0];
+    }
+    
+    return topKElements;
+  }
+}
